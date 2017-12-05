7a64f92f7b811645eda24bd79d3f78458fd70638
Gulmira Berdalieva <gulmira2410@gmail.com>
2017-12-05 12:07:35 -0800

    more db

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
